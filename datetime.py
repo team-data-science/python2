0b1776bc76b2d3d92009293e4352a7fbcf35712f
Team Data Science Andreas Kretz <66935316+team-data-science@users.noreply.github.com>
2021-11-26 09:20:26 +0100

    added todos

---
+++ datetime.py
@@ -0,0 +1,10 @@
+
+# Generate datetime now
+
+# modify datetime now
+
+# generate epoch
+
+# read epoch into datetime and format
+
+# read formated string into datetime and write epoch
---
+++ requests-api.py
@@ -0,0 +1,11 @@
+# Create a very simple API container for the students
+
+# Create GET
+
+# Create POST for URL parameters
+
+# Create POST query string
+
+# CREATE POST body
+
+# Host this container on DockerHub
