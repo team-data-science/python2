39e603c2e5e05bf48892a73f054a1d37e77f4121
Kristijan Bakaric <kbakaric1@gmail.com>
2021-12-06 22:22:06 +0100

    Add numpy.py

---
+++ src/numpy.py
@@ -0,0 +1,11 @@
+# read in dataframe
+import pandas as pd
+# load a csv file
+e_commerce_data_path_csv = "./data/data.csv"
+e_commerce_csv_df = pd.read_csv(
+    e_commerce_data_path_csv,  encoding='unicode_escape', nrows=1000)
+# create new average cloumn of all attributes
+
+# find minimum in dataframe
+
+# find maximum in dataframe
