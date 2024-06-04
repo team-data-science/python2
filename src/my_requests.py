70d0c6d095b91ddbb792104cf46489eaaed649a2
David Reger <davidreger@live.de>
2024-06-04 16:10:31 +0200

    Changed line 37 to whole list instead of one list object

--- src/my_requests.py
+++ src/my_requests.py
@@ -34,7 +34,7 @@ print(disney_data["info"])
 
 # You can also inspect the data key to see the actual characters
 # The data key is a list of dictionaries, where each dictionary represents a character
-print(disney_data["data"][1])
+print(disney_data["data"])
 # >>> [{'_id': 112, 'films': ['Hercules (film)'], 'shortFilms': [], 'tvShows': ['Hercules (TV series)'], 'videoGames': ['Kingdom Hearts III'], 'parkAttractions': [], 'allies': [], 'enemies': [], 'sourceUrl': 'https://disney.fandom.com/wiki/Achilles_(Hercules)', 'name': 'Achilles', 'imageUrl': 'https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png', 'createdAt': '2021-04-12T01:31:30.547Z', 'updatedAt': '2021-12-20T20:39:18.033Z', 'url': 'https://api.disneyapi.dev/characters/112', '__v': 0},
 # >>> {'_id': 18, 'films': ['The Fox and the Hound', 'The Fox and the Hound 2'], 'shortFilms': [], 'tvShows': [], 'videoGames': [], 'parkAttractions': [], 'allies': [], 'enemies': [], 'sourceUrl': 'https://disney.fandom.com/wiki/Abigail_the_Cow', 'name': 'Abigail the Cow', 'imageUrl': 'https://static.wikia.nocookie.net/disney/images/0/05/Fox-disneyscreencaps_com-901.jpg', 'createdAt': '2021-04-12T01:26:03.413Z', 'updatedAt': '2021-12-20T20:39:18.032Z', 'url': 'https://api.disneyapi.dev/characters/18', '__v': 0},
 # >>> ...]
